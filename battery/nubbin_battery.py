b969117ec56c1912c949a1b181b95017b1f0f9d4
deepakjuit <181337@juitsolan.in>
2023-08-06 18:45:50 +0530

    nubbin battery service 4 to 1

--- battery/nubbin_battery.py
+++ battery/nubbin_battery.py
@@ -8,7 +8,7 @@ class NubbinBattery(Battery):
         self.__last_service_date = last_service_date
 
     def needs_service(self):
-        date_which_battery_should_be_serviced_by = add_years_to_date(self.__last_service_date, 4)
+        date_which_battery_should_be_serviced_by = add_years_to_date(self.__last_service_date, 1)
         if date_which_battery_should_be_serviced_by < self.__current_date:
             return True
         else:
