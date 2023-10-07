0edf4ccf8f57036466b5bdae7fa9913b055eea14
Nitish Bahuguna <68819413+N-koder@users.noreply.github.com>
2023-10-07 11:47:05 +0530

    Update engine.py
    complete the function need_services

--- engine/engine.py
+++ engine/engine.py
@@ -1,6 +1,11 @@
 from abc import ABC
-
+from datetime import datetime
 
 class Engine(ABC):
     def needs_service(self):
-        pass
+        current_date = datetime.now()
+        # Check if the car needs service based on some logic
+        # For example, we'll assume it needs service if more than 365 days have passed since the last service
+        days_since_last_service = (current_date - self.last_service_date).days
+        return days_since_last_service > 365
+
